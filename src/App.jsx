f5f61708d33057c95a9892debbc933463d7646ae
Amanda Berwig <amanda.berwig@gmail.com>
2025-04-27 22:03:07 -0300

    Estrutura inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import StoreNavigation from "./components/StoreNavigation";
+import PromoSection from "./components/PromoSection";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div>
+      <StoreNavigation />
+      <PromoSection />
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
